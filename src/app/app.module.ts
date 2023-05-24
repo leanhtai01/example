e37ebd8aa92e10dd4eed9eac7b6ee47fa4ac8261
leanhtai01 <leanhtai01@gmail.com>
2023-05-24 16:13:14 +0700

    successfully build the tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MultiExpansionSelectComponent } from './multi-expansion-select/multi-expansion-select.component';
+import {MatTreeModule} from "@angular/material/tree";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { MultiExpansionSelectComponent } from './multi-expansion-select/multi-ex
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/multi-expansion-select/multi-expansion-select.component.ts
+++ src/app/multi-expansion-select/multi-expansion-select.component.ts
@@ -1,10 +1,227 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {FlatTreeControl} from "@angular/cdk/tree";
+import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
+import {SelectionModel} from "@angular/cdk/collections";
+
+/**
+ * Node for item
+ */
+export class ItemNode {
+  constructor(public item: string, public children: ItemNode[]) {}
+}
+
+/**
+ * Flat item node with expandable and level information
+ */
+export class ItemFlatNode {
+  constructor(
+    public item: string,
+    public level: number,
+    public expandable: boolean
+  ) {}
+}
+
+/**
+ * Json object for list data
+ */
+const TREE_DATA = {
+  'Layer 1': {
+    'Le Anh Tai': null,
+    'Le Anh Tri': null,
+  },
+  'Layer 2': {
+    'Le Van Tuan': null,
+    'Tran Thi Thanh Huyen': null,
+  },
+};
+
+/**
+ * Checklist database, it can build a tree structured Json object.
+ */
+@Injectable()
+export class ChecklistDatabase {
+  dataChange = new BehaviorSubject<ItemNode[]>([]);
+
+  get data(): ItemNode[] {
+    return this.dataChange.value;
+  }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Build the tree nodes from Json object. The result is a list of `TodoItemNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(TREE_DATA, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `TodoItemNode`.
+   */
+  buildFileTree(obj: { [key: string]: any }, level: number): ItemNode[] {
+    return Object.keys(obj).reduce<ItemNode[]>((accumulator, key) => {
+      const value = obj[key];
+      const node = new ItemNode('', []);
+      node.item = key;
+
+      if (value != null) {
+        if (typeof value === 'object') {
+          node.children = this.buildFileTree(value, level + 1);
+        } else {
+          node.item = value;
+        }
+      }
+
+      return accumulator.concat(node);
+    }, []);
+  }
+}
 
 @Component({
   selector: 'app-multi-expansion-select',
   templateUrl: './multi-expansion-select.component.html',
-  styleUrls: ['./multi-expansion-select.component.css']
+  styleUrls: ['./multi-expansion-select.component.css'],
+  providers: [ChecklistDatabase]
 })
 export class MultiExpansionSelectComponent {
+  /** Map from flat node to nested node. This helps us finding the nested node to be modified */
+  flatNodeMap = new Map<ItemFlatNode, ItemNode>();
 
+  /** Map from nested node to flattened node. This helps us to keep the same object for selection */
+  nestedNodeMap = new Map<ItemNode, ItemFlatNode>();
+
+  treeControl: FlatTreeControl<ItemFlatNode>;
+
+  treeFlattener: MatTreeFlattener<ItemNode, ItemFlatNode>;
+
+  dataSource: MatTreeFlatDataSource<ItemNode, ItemFlatNode>;
+
+  /** The selection for checklist */
+  checklistSelection = new SelectionModel<ItemFlatNode>(true /* multiple */);
+
+  constructor(private _database: ChecklistDatabase) {
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren,
+    );
+    this.treeControl = new FlatTreeControl<ItemFlatNode>(this.getLevel, this.isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+    _database.dataChange.subscribe(data => {
+      this.dataSource.data = data;
+    });
+  }
+
+  getLevel = (node: ItemFlatNode) => node.level;
+
+  isExpandable = (node: ItemFlatNode) => node.expandable;
+
+  getChildren = (node: ItemNode): ItemNode[] => node.children;
+
+  hasChild = (_: number, _nodeData: ItemFlatNode) => _nodeData.expandable;
+
+  hasNoContent = (_: number, _nodeData: ItemFlatNode) => _nodeData.item === '';
+
+  /**
+   * Transformer to convert nested node to flat node. Record the nodes in maps for later use.
+   */
+  transformer = (node: ItemNode, level: number) => {
+    const existingNode = this.nestedNodeMap.get(node);
+    const flatNode =
+      existingNode && existingNode.item === node.item ? existingNode : new ItemFlatNode('', 0, false);
+    flatNode.item = node.item;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children?.length;
+    this.flatNodeMap.set(flatNode, node);
+    this.nestedNodeMap.set(node, flatNode);
+    return flatNode;
+  };
+
+  /** Whether all the descendants of the node are selected. */
+  descendantsAllSelected(node: ItemFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    return descendants.length > 0 &&
+      descendants.every(child => {
+        return this.checklistSelection.isSelected(child);
+      });
+  }
+
+  /** Whether part of the descendants are selected */
+  descendantsPartiallySelected(node: ItemFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    const result = descendants.some(child => this.checklistSelection.isSelected(child));
+    return result && !this.descendantsAllSelected(node);
+  }
+
+  /** Toggle the to-do item selection. Select/deselect all the descendants node */
+  todoItemSelectionToggle(node: ItemFlatNode): void {
+    this.checklistSelection.toggle(node);
+    const descendants = this.treeControl.getDescendants(node);
+    this.checklistSelection.isSelected(node)
+      ? this.checklistSelection.select(...descendants)
+      : this.checklistSelection.deselect(...descendants);
+
+    // Force update for the parent
+    descendants.forEach(child => this.checklistSelection.isSelected(child));
+    this.checkAllParentsSelection(node);
+  }
+
+  /** Toggle a leaf to-do item selection. Check all the parents to see if they changed */
+  todoLeafItemSelectionToggle(node: ItemFlatNode): void {
+    this.checklistSelection.toggle(node);
+    this.checkAllParentsSelection(node);
+  }
+
+  /* Checks all the parents when a leaf node is selected/unselected */
+  checkAllParentsSelection(node: ItemFlatNode): void {
+    let parent: ItemFlatNode | null = this.getParentNode(node);
+    while (parent) {
+      this.checkRootNodeSelection(parent);
+      parent = this.getParentNode(parent);
+    }
+  }
+
+  /** Check root node checked state and change it accordingly */
+  checkRootNodeSelection(node: ItemFlatNode): void {
+    const nodeSelected = this.checklistSelection.isSelected(node);
+    const descendants = this.treeControl.getDescendants(node);
+    const descAllSelected =
+      descendants.length > 0 &&
+      descendants.every(child => {
+        return this.checklistSelection.isSelected(child);
+      });
+    if (nodeSelected && !descAllSelected) {
+      this.checklistSelection.deselect(node);
+    } else if (!nodeSelected && descAllSelected) {
+      this.checklistSelection.select(node);
+    }
+  }
+
+  /* Get the parent node of a node */
+  getParentNode(node: ItemFlatNode): ItemFlatNode | null {
+    const currentLevel = this.getLevel(node);
+
+    if (currentLevel < 1) {
+      return null;
+    }
+
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+
+      if (this.getLevel(currentNode) < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
 }
